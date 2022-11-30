04b13e7d1be9874b9558b257a4ae715ed28c8e90
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2022-11-30 15:31:33 +0200

    shopping cart delete function done

--- src/cart.js
+++ src/cart.js
@@ -24,16 +24,20 @@ let calculation = () => {
            <div class="details">
 
              <div class="title-price-x">
-                <h4>
+                <h4 class="title-price">
                   <p>${search.name}</p>
-                  <p>$ ${search.price}</p>
+                  <p class="cart-item-price">$ ${search.price}</p>
                 </h4>
-                <i class="bi bi-x-lg"></i>
+                <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
              </div>
 
-             <div class="cart-buttons"></div>
+             <div class="buttons">
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">${item}</div>
+                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
 
-             <h3></h3>
+             <h3>$ ${item * search.price}</h3>
            </div>
           </div>
         `;
@@ -52,3 +56,52 @@ let calculation = () => {
   };
 
   generateCartItems();
+
+
+  let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+   
+    generateCartItems();
+    update(selectedItem.id);
+
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if(search === undefined) return
+    else if(search.item === 0) return;
+     else {
+        search.item -= 1;
+    }
+   
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    generateCartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+   document.getElementById(id).innerHTML = search.item;
+   calculation(); 
+};
+
+let removeItem = (id) => {
+    let selectedItem = id;
+    basket = basket.filter((x) => x.id !== selectedItem.id);
+    generateCartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
