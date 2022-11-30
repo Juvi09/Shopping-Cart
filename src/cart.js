7d8769de2c444694242d86f26dc742634a104ac1
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2022-11-30 14:46:05 +0200

    shopping cart added and css

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,8 @@
+let label = document.getElementById('label');
+let ShoppingCart = document.getElementById('shopping-cart');
+
+console.log(shopItemsData);
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = () => {
@@ -6,3 +11,44 @@ let calculation = () => {
   };
   
   calculation();
+
+  let generateCartItems = () => {
+     if(basket.length !== 0) {
+       // console.log("basket is not empty");
+       return (ShoppingCart.innerHTML = basket.map((x) => {
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+          <div class="cart-item">
+           <img width="100" src=${search.img} alt="" />
+           <div class="details">
+
+             <div class="title-price-x">
+                <h4>
+                  <p>${search.name}</p>
+                  <p>$ ${search.price}</p>
+                </h4>
+                <i class="bi bi-x-lg"></i>
+             </div>
+
+             <div class="cart-buttons"></div>
+
+             <h3></h3>
+           </div>
+          </div>
+        `;
+       })
+       .join(""));
+    } else {
+        //console.log("basket is totally empty");
+        ShoppingCart.innerHTML = "";
+        label.innerHTML = `
+        <h2>Cart is Empty</h2>
+        <a href="index.html">
+          <button class="HomeBtn">Back to home</button>
+        </a>
+        `;
+     }
+  };
+
+  generateCartItems();
