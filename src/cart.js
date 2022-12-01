df1b3baa0c0f0ef25404a85e0955830f43c6fac7
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2022-12-01 12:14:27 +0200

    Added more images and styling project complete

--- src/cart.js
+++ src/cart.js
@@ -18,15 +18,16 @@ let calculation = () => {
        return (ShoppingCart.innerHTML = basket.map((x) => {
         let { id, item } = x;
         let search = shopItemsData.find((y) => y.id === id) || [];
+        let {img, name, price} = search;
         return `
           <div class="cart-item">
-           <img width="100" src=${search.img} alt="" />
+           <img width="100" src=${img} alt="" />
            <div class="details">
 
              <div class="title-price-x">
                 <h4 class="title-price">
-                  <p>${search.name}</p>
-                  <p class="cart-item-price">R ${search.price}</p>
+                  <p>${name}</p>
+                  <p class="cart-item-price">R ${price}</p>
                 </h4>
                 <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
              </div>
@@ -105,13 +106,14 @@ let removeItem = (id) => {
     basket = basket.filter((x) => x.id !== selectedItem.id);
     generateCartItems();
     TotalAmount();
-
+    calculation();
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
 let clearCart = () => {
    basket = [];
    generateCartItems();
+   calculation();
    localStorage.setItem("data", JSON.stringify(basket))
 };
 
